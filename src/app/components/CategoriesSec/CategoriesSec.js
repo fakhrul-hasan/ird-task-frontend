304adbd105d7d2ad9efb2493ed1d1a01580e3bc7
fakhrul-hasan <rehmanlikhon@gmail.com>
2023-11-19 13:58:25 +0600

    category get from server

--- src/app/components/CategoriesSec/CategoriesSec.js
+++ src/app/components/CategoriesSec/CategoriesSec.js
@@ -1,14 +1,21 @@
-import React from "react";
 import { BsSearch } from "react-icons/bs";
 import CategorySec from "../CategorySec/CategorySec";
 
-const CategoriesSec = () => {
+export async function getCategories() {
+  const res = await fetch('http://localhost:9000/categories');
+  const projects = await res.json();
+  return projects;
+}
+
+const CategoriesSec = async () => {
+  const categoryData = await getCategories();
+
   return (
-    <>
+    <div className="w-60">
       <div className="bg-primary text-white text-sm py-2 text-center rounded-t-md">
         Categories
       </div>
-      <div className="bg-white p-2 rounded-b-md" style={{ height: 'calc(100vh - 125px)' }}>
+      <div className="bg-white p-2 rounded-b-md overflow-y-scroll" style={{ height: 'calc(100vh - 125px)' }}>
       <div className="relative">
           <input
             type="text"
@@ -17,10 +24,21 @@ const CategoriesSec = () => {
           />
           <BsSearch className="absolute left-2 top-2 rounded" />
         </div>
-      <CategorySec/>
+      <CategorySec categoryData={categoryData?.data}/>
       </div>
-    </>
+    </div>
   );
 };
+
+// export const getStaticProps = async () => {
+//     const res = await fetch('https://localhost:9000/categories');
+//     const categoryData = await res.json();
+//     console.log(categoryData);
+//     return {
+//       props: {
+//         categoryData,
+//       },
+//     }
+// }
 
 export default CategoriesSec;
--- src/app/components/CategorySec/CategorySec.js
+++ src/app/components/CategorySec/CategorySec.js
@@ -3,47 +3,58 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { BsChevronRight } from "react-icons/bs";
 
-const CategorySec = () => {
-  const [categories, setCategories] = useState([
-    {
-      id: 1,
-      name: "Category 1",
-      subcategories: [
-        {
-          id: 11,
-          name: "Subcategory 1.1",
-          links: ["Link 1.1.1", "Link 1.1.2"],
-        },
-        {
-          id: 12,
-          name: "Subcategory 1.2",
-          links: ["Link 1.2.1", "Link 1.2.2"],
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "Category 2",
-      subcategories: [
-        {
-          id: 21,
-          name: "Subcategory 2.1",
-          links: ["Link 2.1.1", "Link 2.1.2"],
-        },
-        {
-          id: 22,
-          name: "Subcategory 2.2",
-          links: ["Link 2.2.1", "Link 2.2.2"],
-        },
-      ],
-    },
-  ]);
+const CategorySec = ({ categoryData }) => {
+  // const [categories, setCategories] = useState([
+  //   {
+  //     id: 1,
+  //     name: "Category 1",
+  //     subcategories: [
+  //       {
+  //         id: 11,
+  //         name: "Subcategory 1.1",
+  //         links: ["Link 1.1.1", "Link 1.1.2"],
+  //       },
+  //       {
+  //         id: 12,
+  //         name: "Subcategory 1.2",
+  //         links: ["Link 1.2.1", "Link 1.2.2"],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Category 2",
+  //     subcategories: [
+  //       {
+  //         id: 21,
+  //         name: "Subcategory 2.1",
+  //         links: ["Link 2.1.1", "Link 2.1.2"],
+  //       },
+  //       {
+  //         id: 22,
+  //         name: "Subcategory 2.2",
+  //         links: ["Link 2.2.1", "Link 2.2.2"],
+  //       },
+  //     ],
+  //   },
+  // ]);
 
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+  const [subCategories, setSubCategories] = useState([]);
 
-  const toggleCategory = (categoryId) => {
-    setSelectedCategory(selectedCategory === categoryId ? null : categoryId);
+  const toggleCategory = async (categoryId) => {
+    if (selectedCategory === categoryId) {
+      setSelectedCategory(null);
+    } else {
+      setSelectedCategory(categoryId);
+      const res = await fetch(
+        `http://localhost:9000/subCategories/${categoryId}`
+      );
+      const subCategories = await res.json();
+      setSubCategories(subCategories?.data);
+      console.log(subCategories?.data);
+    }
     setSelectedSubcategory(null);
   };
 
@@ -54,63 +65,64 @@ const CategorySec = () => {
   };
 
   return (
-    <div className="mt-2">
-      {categories.map((category) => (
-        <div key={category.id} className="mb-2">
+    <div className="mt-2 space-y-3">
+      {categoryData?.map((category) => (
+        <div key={category?.id} className="mb-2">
           <div
             className={`cursor-pointer flex items-center justify-between p-1 rounded-md ${
-              selectedCategory === category?.id
-                ? "bg-gray-200"
-                : ""
+              selectedCategory === category?.cat_id ? "bg-gray-200" : ""
             }`}
-            onClick={() => toggleCategory(category.id)}
+            onClick={() => toggleCategory(category?.cat_id)}
           >
             <div className="flex gap-1">
               <Image src="/Frame.png" alt="category" height={30} width={30} />
               <div>
-                <h6 className="text-sm">{category.name}</h6>
-                <p className="text-gray-600 text-xs">Subcategories: 7</p>
+                <h6 className="text-sm font-medium">{category?.cat_name_en}</h6>
+                <p className="text-gray-600 text-xs">
+                  Subcategories: {category?.no_of_subcat}
+                </p>
               </div>
             </div>
             <div className="flex flex-col items-center">
-                <h5 className="text-sm">21</h5>
-                <p className="text-xs text-gray-600">Duas</p>
+              <h5 className="text-sm">{category?.no_of_dua}</h5>
+              <p className="text-xs text-gray-600">Duas</p>
             </div>
           </div>
-          {selectedCategory === category.id && (
+          {selectedCategory === category?.cat_id && (
             <div className="ml-5 border-l-2 border-dotted border-primary">
-              {category.subcategories.map((subcategory) => (
-                <div key={subcategory.id} className="mb-2 cursor-pointer">
-                  <div
-                    className="ml-[5px] flex items-center relative pl-2"
-                    onClick={() => toggleSubcategory(subcategory?.id)}
-                  >
-                    <div className="w-1.5 h-1.5 bg-primary rounded-full absolute -left-[9px]" />
-                    <h5
-                      className={`${
-                        selectedSubcategory === subcategory?.id
-                          ? "text-primary"
-                          : ""
-                      }`}
+              {subCategories?.length > 0 &&
+                subCategories?.map((subcategory) => (
+                  <div key={subcategory.id} className="mb-2 cursor-pointer">
+                    <div
+                      className="ml-[5px] flex items-center relative pl-2"
+                      onClick={() => toggleSubcategory(subcategory?.id)}
                     >
-                      {subcategory.name}
-                    </h5>
-                  </div>
-                  {selectedSubcategory === subcategory.id && (
-                    <div className="">
-                      {subcategory.links.map((link) => (
-                        <div
-                          key={link}
-                          className="mb-2 cursor-pointer ml-6 flex items-center"
-                        >
-                          <BsChevronRight className="text-xs text-primary" />
-                          {link}
-                        </div>
-                      ))}
+                      <div className="w-1.5 h-1.5 bg-primary rounded-full absolute -left-[9px]" />
+                      <h5
+                        className={`${
+                          selectedSubcategory === subcategory?.id
+                            ? "text-primary"
+                            : ""
+                        }`}
+                      >
+                        {subcategory?.subcat_name_en}
+                      </h5>
                     </div>
-                  )}
-                </div>
-              ))}
+                    {selectedSubcategory === subcategory.id && (
+                      <div className="">
+                        {subcategory.links.map((link) => (
+                          <div
+                            key={link}
+                            className="mb-2 cursor-pointer ml-6 flex items-center"
+                          >
+                            <BsChevronRight className="text-xs text-primary" />
+                            {link}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ))}
             </div>
           )}
         </div>
